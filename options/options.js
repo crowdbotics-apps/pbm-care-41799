c7f05da50f44c891310ba3024199e1465dd0b30e
Crowdbotics <team@crowdbotics.com>
2023-06-29 05:45:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pbm-care-41799",
+  url: "https://pbm-care-41799.botics.co",
+  api: "https://pbm-care-41799.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
